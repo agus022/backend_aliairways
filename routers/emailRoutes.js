626e23e723302f745ac09bfb3916dcc9895e2e97
nath-coder <nathashasevilla@gmail.com>
2025-05-11 23:50:27 -0600

    authentication

--- routers/emailRoutes.js
+++ routers/emailRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { sendEmail } from '../controllers/emailController.js';
+import { checkRole }  from '../middlewares/checkRole.js';
 
 const router = Router();
 
-router.post('/send', sendEmail);
+router.post('/send',checkRole(['admin']), sendEmail);
 
 export default router;
